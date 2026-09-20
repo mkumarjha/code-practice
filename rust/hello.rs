b5d234affdc35ca5e361ccf236605d5194584784
Mithilesh Jha <mithilesh.jha@bdpint.com>
2026-09-20 23:06:02 +0530

    learning_notes

--- rust/hello.rs
+++ rust/hello.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+
+https://www.classcentral.com/classroom/freecodecamp-learn-rust-programming-complete-course-198989
+
+
+https://www.classcentral.com/report/best-rust-courses/#exercism
